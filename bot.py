62ad96f87904b449e863e56477e2c90a8e77e45c
ALVIN MARCO CRISTY TAMPUBOLON <alvinmarco8@gmail.com>
2024-02-27 14:13:28 +0700

    update

--- bot.py
+++ bot.py
@@ -4,44 +4,17 @@ import yfinance as yf
 API_KEY = '7123140191:AAEL-SOG_2Vtnnjn6gfDFSKyGVTYY5vlO2c'
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['Greet'])
+@bot.message_handler(commands=['Salam'])
 def greet(message):
-  bot.reply_to(message, "Hey! Hows it going?")
+  bot.reply_to(message, "Halo, Saya StocksBotID siap membantu anda.")
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
   bot.send_message(message.chat.id, "Hello!")
 
-@bot.message_handler(commands=['wsb'])
-def get_stocks(message):
-  response = ""
-  stocks = ['gme', 'amc', 'nok']
-  stock_data = []
-  for stock in stocks:
-    data = yf.download(tickers=stock, period='2d', interval='1d')
-    data = data.reset_index()
-    response += f"-----{stock}-----\n"
-    stock_data.append([stock])
-    columns = ['stock']
-    for index, row in data.iterrows():
-      stock_position = len(stock_data) - 1
-      price = round(row['Close'], 2)
-      format_date = row['Date'].strftime('%m/%d')
-      response += f"{format_date}: {price}\n"
-      stock_data[stock_position].append(price)
-      columns.append(format_date)
-    print()
-
-  response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
-  for row in stock_data:
-    response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
-  response += "\nStock Data"
-  print(response)
-  bot.send_message(message.chat.id, response)
-
 def stock_request(message):
   request = message.text.split()
-  if len(request) < 2 or request[0].lower() not in "price":
+  if len(request) < 2 or request[0].lower() not in "cek":
     return False
   else:
     return True
